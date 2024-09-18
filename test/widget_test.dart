25ddb6ddc8c003e7dc40c3f2367fb420d108ceb9
Paulo Marques <pjp.marques@gmail.com>
2024-09-18 22:39:56 +0100

    Changed the application identifier to 'pmarques.eu.terrabearing'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mountainid/main.dart';
+import 'package:terrabearing/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
